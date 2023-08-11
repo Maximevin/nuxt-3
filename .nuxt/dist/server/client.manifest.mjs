4913fbdb8026719e7c283506481948ac66982b58
MaxFar92 <maxime@farandole.io>
2023-08-11 10:53:41 +0200

    ajout vue dependencies

---
+++ .nuxt/dist/server/client.manifest.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///Users/maximehmc/MasteringNuxt/nuxt-3/node_modules/.pnpm/@nuxt+vite-builder@3.0.0_vue@3.3.4/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,13 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss', '@vueuse/nuxt'],
-  build: { 
+  vite: {
+    build: { 
       rollupOptions : {
-        external: 'NonExistingPath'
+        external: [
+          'vue',
+        ],
       }
     }
+  } 
     });
