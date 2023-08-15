e2680fc02538eb31bc3e0dd0429cdf158f693679
MaxFar92 <maxime@farandole.io>
2023-08-15 23:59:40 +0200

    middleware issue

--- .nuxt/types/middleware.d.ts
+++ .nuxt/types/middleware.d.ts
@@ -1,5 +1,5 @@
 import type { NavigationGuard } from 'vue-router'
-export type MiddlewareKey = string
+export type MiddlewareKey = "auth"
 declare module "/Users/maximehmc/MasteringNuxt/nuxt-3/node_modules/.pnpm/nuxt@3.0.0_rollup@2.79.1/node_modules/nuxt/dist/pages/runtime/composables" {
   interface PageMeta {
     middleware?: MiddlewareKey | NavigationGuard | Array<MiddlewareKey | NavigationGuard>
